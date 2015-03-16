b510f12ca11336338cdf5baea5f230f107b4ab40
s-a <stephan.ahlf@googlemail.com>
2015-03-16 19:08:27 +0100

    efx addons

---
+++ public/assets/js/tests.js
@@ -0,0 +1,31 @@
+(function() {
+
+	var test = function  () {
+		if (navigator.userAgent.indexOf('SlimerJS') !== -1) {
+
+			describe("simple test", function() {
+			    it("should test", function(){
+					assert(true, "1!==1");
+			    });
+			});
+
+			describe("Studio", function() {
+			    it("should should load project", function(done){
+		    		window.studio.init(defaultProject, function(config) { 
+						assert(window.studio.project._el.state.devices.length === 1, "1!==1");
+		    			done(); 
+		    		});
+
+			    });
+			});
+			var runner = window.mocha.run();
+		} else {
+			if (document.location.hostname === "localhost"){
+				window.studio.init(defaultProject);
+			}
+		}
+	};
+
+
+	window.boomTestSuite = test;
+})();
